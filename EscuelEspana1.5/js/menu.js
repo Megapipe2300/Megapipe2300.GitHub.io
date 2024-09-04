c11da33d033307ffba4dce44372c01d71571527f
Megapipe2300 <f.devia.c@gmail.com>
2024-09-04 11:19:05 -0400

    Add files via upload

---
+++ EscuelEspana1.5/js/menu.js
@@ -0,0 +1,13 @@
+const che = document.querySelector('.drawer');
+const menu = document.querySelector('.menu-box')
+
+
+che.addEventListener("click", cerrar );
+menu.addEventListener("click", cerrar );
+
+function cerrar()
+{
+	
+	
+	
+} 
---
+++ EscuelEspana1.5/js/scrol.js
@@ -0,0 +1,27 @@
+/* SCROLL*/
+
+ // Obtener todos los botones con la clase "miBoton"
+    var botones = document.getElementsByClassName("miBoton");
+    var botones = document.getElementsByClassName("boton");
+	
+    // Recorrer todos los botones y agregar el event listener a cada uno
+    for (var i = 0; i < botones.length; i++) {
+        botones[i].addEventListener("click", function() {
+            // Desplazarse a la posición (300, 500) en la ventana
+            window.scrollTo(300, 500);
+        });
+    }
+	
+	
+	 // Obtener el primer botón con la clase "miBoton"
+    var botone = document.getElementsByClassName("boton")[0];
+
+    // Agregar un event listener para el evento "click"
+    buton.addEventListener("click", function() {
+        // Desplazarse a la posición (300, 500) en la ventana
+        window.scrollTo(200, 500);
+    });
+	
+
+
+    
